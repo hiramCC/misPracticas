fbc3032e28f1f9f11fa3e65a99afe3ec30191fac
hiramCC <hiram_102499@hotmail.com>
2023-01-26 16:31:27 -0600

    calcu casi final

--- application/views/view_practica.php
+++ application/views/view_practica.php
@@ -14,15 +14,15 @@
 
     <div class="container">
         <h1>Calculadora monumental del olimpo en JQuery</h1>
-        <form>
+        <form autocomplete="false" id="myForm">
 
-            <label class="radio-inline"><input type="radio" id="sumar" name="opt" checked>Sumar</label>
+            <label class="radio-inline"><input type="radio" id="sumar" name="opt">Sumar</label>
             <label class="radio-inline"><input type="radio" id="restar" name="opt">Restar</label>
             <label class="radio-inline"><input type="radio" id="multiplicar" name="opt">Multiplicar</label>
             <label class="radio-inline"><input type="radio" id="dividir" name="opt">Dividir</label>
             <div class="form-group">
                 <label for="numero1">Numero 1</label>
-                <input type="number" class="form-control" id="numero1">
+                <input type="number"  class="form-control" id="numero1">
             </div>
             <div class="form-group">
                 <label for="numero2">Numero 2</label>
@@ -31,7 +31,8 @@
 
             <button type="button" onclick="operaciones()">Aceptar</button>
         </form>
-        <p id="sumando"></p>
+        <div id="sumando"></div>
+        <p id="test"></p>
     </div>
 
 </body>
@@ -51,25 +52,45 @@ function operaciones() {
     var x, y, suma, resta, multiplica, divide;
     x = parseFloat(document.getElementById("numero1").value);
     y = parseFloat(document.getElementById("numero2").value);
+    
+    if($("#numero1").empty() || $("#numero2").empty() ){
+        document.getElementById("sumando").innerHTML = '<div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
+        let reset = setTimeout(clear, 3000);
     if ($('#sumar').is(':checked')) {
-        suma = parseFloat(x) + parseFloat(y);
-        document.getElementById("sumando").innerHTML = suma;
-        alert(suma);
+        if((isset($("#numero1")) || isset($("#numero1"))) && ($("#numero1").empty() || $("#numero2").empty() )){
+            document.getElementById("sumando").innerHTML = '<div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
+            let reset = setTimeout(clear, 3000);
+        }else{
+            suma = parseFloat(x) + parseFloat(y);
+            document.getElementById("sumando").innerHTML = '<div class="alert alert-success"><strong> El resultado es :'+suma+ '</strong></div>';
+            let reset = setTimeout(clear, 3000);
+        //alert(suma);
+        }
+        
     } else if($('#restar').is(':checked')){
         resta = parseFloat(x) - parseFloat(y);
-        document.getElementById("sumando").innerHTML = resta;
-        alert(resta);
+        document.getElementById("sumando").innerHTML = '<div class="alert alert-success"><strong> El resultado es :'+resta+'</strong></div>';
+        let reset = setTimeout(clear, 3000);
+        //alert(resta);
     } else if($('#multiplicar').is(':checked')){
         multiplica = parseFloat(x) * parseFloat(y);
-        document.getElementById("sumando").innerHTML = multiplica;
-        alert(multiplica);
+        document.getElementById("sumando").innerHTML = '<div class="alert alert-success"><strong> El resultado es :'+multiplica+ '</strong></div>';
+        let reset = setTimeout(clear, 3000);
+        //alert(multiplica);
     } else if($('#dividir').is(':checked')){
         divide = parseFloat(x) / parseFloat(y);
-        document.getElementById("sumando").innerHTML = divide;
-        alert(divide);
-    } 
+        document.getElementById("sumando").innerHTML = '<div class="alert alert-success"><strong> El resultado es :'+divide+ '</strong></div>';
+        let reset = setTimeout(clear, 3000);
+        //alert(divide);
+    }
 }
+    document.getElementById("myForm").reset();
+    //document.getElementById("sumando").reset();
 
+}
+function clear(){
+    document.getElementById("sumando").innerHTML = '';
+}
 function myFunction() {
     var x, y, suma;
     x = document.getElementById("numero1").value;
