cdbe114e732b07647fd7c36107e3242d842e524c
hiramCC <hiram_102499@hotmail.com>
2023-01-27 16:46:48 -0600

    changes

--- application/controllers/practicas.php
+++ application/controllers/practicas.php
@@ -13,7 +13,7 @@ class practicas extends CI_Controller{
     }
     public function registrar(){
 
-        /* 
+         /* 
         1.- Corresponde al campo en el front
         2.- Dato que se insertara como mensaje de error
         3.- Regla y/o reglas a validar
@@ -33,8 +33,8 @@ class practicas extends CI_Controller{
                 'rules' => 'required|trim|max_length[30]|regex_match[/^[a-zñáéíóúüA-ZÑÁÉÍÓÚÜ ,.]*$/u]'
             ),
             array(
-                'field' => 'email', 'label' => 'Dirección de correo',
-                'rules' => 'required|trim|valid_email|is_unique[preregistro.correo]'
+                'field' => 'email', 'label' => 'Email',
+                'rules' => 'required|trim|valid_email|is_unique[preregistro.email]'
             ),
             array(
                 'field' => 'pwd', 'label' => 'Contraseña',
@@ -47,7 +47,7 @@ class practicas extends CI_Controller{
         );
         //$this->form_validation->set_rules('nombre', 'nombre', 'required|trim');
         $this->form_validation->set_rules($config);
-        $this->form_validation->set_error_delimiters('<p class="text-danger">', '</p>');
+        $this->form_validation->set_error_delimiters('<p class="text-danger" id="message">', '</p>');
         if ($this->form_validation->run() == FALSE) {
 
             $messageData['success'] = false;
@@ -59,21 +59,12 @@ class practicas extends CI_Controller{
                 'nombre' => trim($this->input->post('nombre')),
                 'apaterno' => $this->input->post('apaterno'),
                 'amaterno' => $this->input->post('amaterno'),
-                'correo' => $this->input->post('email'),
-                'contrasenia' => $this->input->post('pwd')
+                'email' => $this->input->post('email'),
+                'password' => $this->input->post('pwd')
             );
-            exit();
-            $result = $this->registro_model->insert($datos);
-            if ($result == TRUE) {
-                $messageData["success"] = true;
-                $messageData["messages"]='<div class="alert alert-success alert-dismissible"></div>';
-            } else {
-                $messageData["success"] = true;
-                $messageData["messages"]='<div class="alert alert-warning alert-dismissible"></div>';
-            
-            }
+                $messageData['success'] = true;
+                $messageData['messages'] = "<div class=\"alert alert-success\"><strong>Registro exitoso!</strong></div>";
         }
-
         echo json_encode($messageData);
     }
 }
--- application/views/view_practica.php
+++ application/views/view_practica.php
@@ -11,200 +11,183 @@
 </head>
 
 <body>
-
-    <div class="container">
-        <div class="col-sm-6">
-            <h1>Calculadora monumental del olimpo en JQuery</h1>
-            <form autocomplete="false" id="myForm">
-
-                <label class="radio-inline"><input type="radio" id="sumar" name="opt">Sumar</label>
-                <label class="radio-inline"><input type="radio" id="restar" name="opt">Restar</label>
-                <label class="radio-inline"><input type="radio" id="multiplicar" name="opt">Multiplicar</label>
-                <label class="radio-inline"><input type="radio" id="dividir" name="opt">Dividir</label>
-                <div class="form-group">
-                    <label for="numero1">Numero 1</label>
-                    <input type="number" class="form-control" id="numero1">
-                </div>
-                <div class="form-group">
-                    <label for="numero2">Numero 2</label>
-                    <input type="number" class="form-control" id="numero2">
-                </div>
-
-                <button type="button" onclick="operaciones()">Aceptar</button>
-            </form>
-            <div id="sumando"></div>
-            <p id="test"></p>
-            <br>
-            <br>
-        </div>
-    </div>
-    <div class="row">
-                <div class="col-sm-6">
-                    <span id="msg-data"></span>
-                    <h2>Validando datos</h2>
-                    <form id="frmRegistro">
-                        <?php echo validation_errors(); ?>
-                        <div class="form-group">
-                            <label for="nombre">Nombre:</label>
-                            <input type="text" class="form-control" id="nombre" name="nombre">
-                        </div>
-                        <div class="form-group">
-                            <label for="apaterno">Apellido Paterno:</label>
-                            <input type="text" class="form-control" id="apaterno" name="apaterno">
-                        </div>
-                        <div class="form-group">
-                            <label for="amaterno">Apellido Materno:</label>
-                            <input type="text" class="form-control" id="amaterno" name="amaterno">
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email:</label>
-                            <input type="email" class="form-control" id="email" name="email">
-                        </div>
-                        <div class="form-group">
-                            <label for="pwd">Password:</label>
-                            <input type="password" class="form-control" id="pwd" name="pwd">
-                        </div>
-                        <div class="form-group">
-                            <label for="pwd2">Password:</label>
-                            <input type="password" class="form-control" id="pwd2" name="pwd2">
-                        </div>
-                        <button type="submit" class="btn btn-success">Guardar</button>
-                    </form>
-                </div>
-                <div class="col-sm-6"></div>
+<div class="container">
+        <h1>Calculadora</h1>
+        <form autocomplete="false" id="myForm">
+            <label class="radio-inline"><input type="radio" id="sumar" name="opt">Sumar</label>
+            <label class="radio-inline"><input type="radio" id="restar" name="opt">Restar</label>
+            <label class="radio-inline"><input type="radio" id="multiplicar" name="opt">Multiplicar</label>
+            <label class="radio-inline"><input type="radio" id="dividir" name="opt">Dividir</label>
+            <div class="form-group">
+                <label for="numero1">Numero 1</label>
+                <input type="number" class="form-control" id="n1">
             </div>
+            <div class="form-group">
+                <label for="numero2">Numero 2</label>
+                <input type="number" class="form-control" id="n2">
+            </div>
+            <button type="button" onclick="operaciones()">Aceptar</button>
+        </form>
+        <div id="sumando"></div>
+        <p id="test"></p>
+    </div>
+    <br>
+    <div class="container" style="margin-top:50px;">
+        <!-- <p> <?php #echo $this->session->flashdata('success')?> </p> -->
+        <h2>Registro de Participante </h2>
+        <span id="msg-data"></span>
+        <form id="frmRegistro">
+            <!-- <?php #echo validation_errors(); ?> -->
+            <div class="form-group">
+
+                <label for="nombre">Nombre:</label>
+                <input type="text" class="form-control" id="nombre" name="nombre">
+            </div>
+            <div class="form-group">
+                <label for="apaterno">Apellido Paterno:</label>
+                <input type="text" class="form-control" id="apaterno" name="apaterno">
+            </div>
+            <div class="form-group">
+                <label for="amaterno">Apellido Materno:</label>
+                <input type="text" class="form-control" id="amaterno" name="amaterno">
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" name="email">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Contraseña:</label>
+                <input type="password" class="form-control" id="pwd" name="pwd">
+            </div>
+            <div class="form-group">
+                <label for="pwd2">Contraseña:</label>
+                <input type="password" class="form-control" id="pwd2" name="pwd2">
+            </div>
+            <button type="submit" class="btn btn-success">Guardar</button>
+        </form>
+
+
     </div>
 </body>
 
 </html>
-
 <script>
 $(document).ready(function() {
-    //alert()
-    //var numero1 = document.getElementById("numero1").value; == //var contents = $('#numero1')[0];
-    //alert(numero1);
-
-    //alert(contents);
-
     $('#frmRegistro').on("submit", function(e) {
         e.preventDefault();
         let dataString = $('#frmRegistro').serialize();
+        // alert(dataString)
         $.ajax({
-            url: "<?php echo base_url('practicas/registrar')?>",
+            url: "<?php echo base_url('practicas/registrar') ?>",
             method: 'POST',
             data: dataString,
             success: function(response) {
-                $('.form-group').removeClass('has-error').removeClass('has-success');
-                $('.text-danger').remove();
+                $(".text-danger").remove();
+                document.getElementById('frmRegistro').reset();
                 var data = JSON.parse(response);
-                console.log(data.success);
-                if(data.success == true){
+                if (data.success == true) {
                     $('#msg-data').html(data.messages);
-                    limpiar();
-                }else{
-                    $.each(data.messages, function(key,value){
-                        var element = $('#' + key);
-                        element.closest('div.form-group')
-                        .removeClass('has-error')
-                        .addClass(value.length > 0 ? 'has-error' : 'has-success')
-                        .find('.text text-danger')
-                        .remove();
-                        element.after(value);
+                    limpiarForm();
+                } else {
+
+                    $.each(data.messages, function(key, value) {
+                        let element = $("#" + key);
+                        let div = element.closest(".form-group");
+                        div.after(value);
                     });
+
                 }
             }
-
-        });
+        })
     });
 
+    function limpiarForm() {
+        if ($('#msg-data').is(':visible')) {
+            setTimeout(function() {
+                $('#msg-data').hide('hide');
+            }, 2000);
+        } else {
+            $('#msg-data').show();
+            setTimeout(function() {
+                $('#msg-data').hide('hide');
+            }, 2000);
+        }
+        
+        document.getElementById('frmRegistro').reset();
+    }
 });
-function limpiar(){
-    setTimeout(function(){
-       $("#msg-data").hide("slow");
-       //$("·msg-data").html(''); 
-    }, 2000);
-    $('#frmRegistro')[0].reset();
-}
 /* 
 function operaciones() {
     var x, y, suma, resta, multiplica, divide;
-    x = parseFloat(document.getElementById("numero1").value);
-    y = parseFloat(document.getElementById("numero2").value);
-
-    if ($("#numero1").empty() || $("#numero2").empty()) {
-        //document.getElementById("sumando").innerHTML = '<div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
-        //let reset = setTimeout(clear, 3000);
+    x = parseFloat(document.getElementById("n1").value);
+    y = parseFloat(document.getElementById("n2").value);
+    if ($("#n1").empty() || $("#n2").empty()) {
         if ($('#sumar').is(':checked')) {
-            if (!$('#numero1').val() || !$('#numero2').val()) {
+            if (!$('#n1').val() || !$('#n2').val()) {
                 document.getElementById("sumando").innerHTML =
-                    '<div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
+                    '<br><div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
                 let reset = setTimeout(clear, 3000);
             } else {
                 suma = parseFloat(x) + parseFloat(y);
                 document.getElementById("sumando").innerHTML =
-                    '<div class="alert alert-success"><strong> El resultado es :' + suma + '</strong></div>';
+                    '<br><div class="alert alert-success"><strong> El resultado es :' + suma + '</strong></div>';
                 let reset = setTimeout(clear, 3000);
                 //alert(suma);
             }
-
         } else if ($('#restar').is(':checked')) {
-            if (!$('#numero1').val() || !$('#numero2').val()) {
+            if (!$('#n1').val() || !$('#n2').val()) {
                 document.getElementById("sumando").innerHTML =
-                    '<div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
+                    '<br><div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
                 let reset = setTimeout(clear, 3000);
             } else {
                 resta = parseFloat(x) - parseFloat(y);
                 document.getElementById("sumando").innerHTML =
-                    '<div class="alert alert-success"><strong> El resultado es :' + resta + '</strong></div>';
+                    '<br><div class="alert alert-success"><strong> El resultado es :' + resta + '</strong></div>';
                 let reset = setTimeout(clear, 3000);
-                //alert(resta);
             }
         } else if ($('#multiplicar').is(':checked')) {
-            if (!$('#numero1').val() || !$('#numero2').val()) {
+            if (!$('#n1').val() || !$('#n2').val()) {
                 document.getElementById("sumando").innerHTML =
-                    '<div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
+                    '<br><div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
                 let reset = setTimeout(clear, 3000);
             } else {
                 multiplica = parseFloat(x) * parseFloat(y);
                 document.getElementById("sumando").innerHTML =
-                    '<div class="alert alert-success"><strong> El resultado es :' + multiplica + '</strong></div>';
+                    '<br><div class="alert alert-success"><strong> El resultado es :' + multiplica + '</strong></div>';
                 let reset = setTimeout(clear, 3000);
                 //alert(multiplica);
             }
         } else if ($('#dividir').is(':checked')) {
-            if (!$('#numero1').val() || !$('#numero2').val()) {
+            if (!$('#n1').val() || !$('#n2').val()) {
                 document.getElementById("sumando").innerHTML =
-                    '<div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
+                    '<br><div class="alert alert-danger"><strong> Metele datos papi </strong></div>';
                 let reset = setTimeout(clear, 3000);
             } else {
                 divide = parseFloat(x) / parseFloat(y);
                 document.getElementById("sumando").innerHTML =
-                    '<div class="alert alert-success"><strong> El resultado es :' + divide + '</strong></div>';
+                    '<br><div class="alert alert-success"><strong> El resultado es :' + divide + '</strong></div>';
                 let reset = setTimeout(clear, 3000);
                 //alert(divide);
             }
         } else {
             document.getElementById("sumando").innerHTML =
-                '<div class="alert alert-warning"><strong> Selecciona que operacion quieres realizar </strong></div>';
+                '<br><div class="alert alert-warning"><strong> Selecciona que operacion quieres realizar </strong></div>';
             let reset = setTimeout(clear, 4000);
         }
     }
     document.getElementById("myForm").reset();
     //document.getElementById("sumando").reset();
-
 }
-
 function clear() {
     document.getElementById("sumando").innerHTML = '';
 }
-
 function myFunction() {
     var x, y, suma;
-    x = document.getElementById("numero1").value;
-    y = document.getElementById("numero2").value;
+    x = document.getElementById("n1").value;
+    y = document.getElementById("n2").value;
     suma = parseFloat(x) + parseFloat(y);
     document.getElementById("sumando").innerHTML = suma;
     alert(suma);
-}
-*/
+} */
 </script>
+
